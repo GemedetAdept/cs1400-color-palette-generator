e6828b3e896546bc26b2b8254eee709c36d66581
Vivian Zitek <vivian.zitek@gmail.com>
2022-10-19 23:26:04 -0600

    Add arrow key functionality to input menu.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
 bool inputMenu = true;
 
 
-string cursorIdle = "> ";
-string cursorActive = ">>> ";
+string cursorIdle = ">";
+string cursorActive = ">>>";
 string[] inputMenuCursors = {
 	cursorIdle,
 	cursorIdle,
@@ -16,12 +16,34 @@ string[] inputMenuCursors = {
 	cursorIdle,
 };
 
+int optionActive = 0;
+
 while (inputMenu) {
 
+	Console.Clear();
+
+	// Reset all option cursors to default
+	for (int i = 0; i < inputMenuCursors.Length; i++) {
+		inputMenuCursors[i] = cursorIdle;
+		inputMenuCursors[optionActive] = cursorActive;
+	}
+
+	// Display menu options
 	for (int i = 0; i < inputMenuCursors.Length; i++) {
 		Console.WriteLine($"{inputMenuCursors[i]} {(ColorInput)i}");
 	}
-	Console.ReadKey();
+
+	// Change active option using the Up and Down arrow keys
+	var arrowKeyInput = Console.ReadKey(false).Key;
+	if (arrowKeyInput == ConsoleKey.UpArrow && optionActive > 0){
+		optionActive -= 1;
+	}
+	else if (arrowKeyInput == ConsoleKey.DownArrow && optionActive < (inputMenuCursors.Length-1)) {
+		optionActive += 1;
+	}
+	else {
+		optionActive = optionActive;
+	}
 }
 
 
