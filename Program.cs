9a8c1adaf6e696c5d94e3b93932397a1c7a4c026
Vivian Zitek <vivian.zitek@gmail.com>
2022-10-19 21:53:26 -0600

    Add preliminary user input menu.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
 ﻿// CS 1400, Final Project - Color Palette Generator
 // By: Jasmine Carrasco, Chad Miller, and Pluto Zitek
+
+bool inputMenu = true;
+
+
+string cursorIdle = "> ";
+string cursorActive = ">>> ";
+string[] inputMenuCursors = {
+	cursorIdle,
+	cursorIdle,
+	cursorIdle,
+	cursorIdle,
+	cursorIdle,
+	cursorIdle,
+	cursorIdle,
+};
+
+while (inputMenu) {
+
+	for (int i = 0; i < inputMenuCursors.Length; i++) {
+		Console.WriteLine($"{inputMenuCursors[i]} {(ColorInput)i}");
+	}
+	Console.ReadKey();
+}
+
+
+
+enum ColorInput {
+	CMYK,
+	CSSName,
+	Hexadecimal,
+	HexadecimalName,
+	HSL,
+	HSV,
+	RGB,
+}
