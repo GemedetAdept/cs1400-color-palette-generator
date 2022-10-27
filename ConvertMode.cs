af3b95273a1e9a9f10d605ea6fd1346776b34e97
Vivian Zitek <vivian.zitek@gmail.com>
2022-10-27 15:49:39 -0600

    Add color conversion TODO.

---
+++ ConvertMode.cs
@@ -0,0 +1,37 @@
+// Functions for converting between color modes
+
+/* TODO
+- CMYK
+	- CMYK to HEX
+	- CMYK to HSL
+	- CMYK to HSV
+	- CMYK to RGB
+- HEX
+	- HEX to CMYK
+	- HEX to HSL
+	- HEX to HSV
+	- HEX to RGB
+- HSL
+	- HSL to CMYK
+	- HSL to HEX
+	- HSL to HSV
+	- HSL to RGB
+- HSV
+	- HSV to CMYK
+	- HSV to HEX
+	- HSV to HSL
+	- HSV to RGB
+- RGB
+	- RGB to HEX
+	- RGB to CMYK
+	- RGB to HSL
+	- RGB to HSV
+*/
+
+public class ConvertMode {
+
+	public void RGBToHex (int red, int green, int blue) {
+		
+	}
+
+}
