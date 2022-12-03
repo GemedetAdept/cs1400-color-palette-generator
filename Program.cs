9a39a88f0ced3e3c80652d0ef8d44c5c35e16302
Vivian Zitek <vivian.zitek@gmail.com>
2022-12-02 23:19:36 -0700

    Implement base logistic function.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿// CS 1400, Final Project - Color Palette Generator
 // By: Jasmine Carrasco, Chad Miller, and Pluto Zitek
-using UserInput;
+// using UserInput;
 
-UserInput.UserInput.Input();
+// UserInput.UserInput.Input();
 
-// What are the color harmonies?
-ColorHarmony.Colorharmonypick();
-ColorHarmony.ColorHarmonyInfo();
-ColorHarmony.Menu();
+// // What are the color harmonies?
+// ColorHarmony.Colorharmonypick();
+// ColorHarmony.ColorHarmonyInfo();
+// ColorHarmony.Menu();
+
+// (double x, double xOrig, double yOrig, double K, double A, double m, double n)
+Console.WriteLine(
+	ValueDeviation.ClampByLogistic(20.0, 0.0, 0.0, 360, 30, 0.21, 0.27)
+);
--- ValueDeviation.cs
+++ ValueDeviation.cs
@@ -2,18 +2,21 @@ public class ValueDeviation {
 
 	// An implementation of my custom logistic function (https://www.geogebra.org/classic/vmd7cvjj)
 	// Constructed from (https://en.wikipedia.org/wiki/Logistic_function), based on: Verhulst, Pierre-François (1838), Correspondance Mathématique et Physique. Vol. 10: p. 116
-	public static double ClampByLogistic() {
+	public static double ClampByLogistic(double x, double xOrig, double yOrig, double K, double A, double m, double n) {
 
 		// Given:
-		//	x :: Input; value to be fitted to function
+		//	x :: Input; value to be fitted to function, used as a linear coordinate with a y value of 0
 		//	K :: Ceiling value; if the input is greater than K, it is culled to equal K.
 		//	A :: Floor value; if the input is less than A, it is culled to equal A
-		//	k :: Logistic Slope; the rate of value change between A and K
-		//	n :: Slope Modulator; fine-tune the rate of value change without changing the base value k
-		//	x₀ :: x-Origin; x-value of the function origin
-		//	y₀ :: y-Origin; y-value of the function origin
-		//
-		// ⨍(x) = y₀+((K-A)/(1 + e^(-k*x*n-x₀))) + A
+		//	m :: Logistic Slope; the rate of value change between A and K
+		//	n :: Slope Modulator; fine-tune the rate of value change without changing the base value m
+		//	x₀ (xOrig) :: x-Origin; x-value of the function origin
+		//	y₀ (yOrig) :: y-Origin; y-value of the function origin
+		//	e :: Euler's number
+		// ⨍(x) = y₀+((K-A)/(1 + e^(-m*x*n-x₀))) + A
 
+		double logisticOutput = 0.0;
+		logisticOutput = yOrig + ((K-A)/(1 + Math.Pow(Math.E, -m*x*n-xOrig))) + A;
+		return logisticOutput;
 	}
 }
