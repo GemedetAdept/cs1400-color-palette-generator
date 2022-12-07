ad529cac6f81bfa47f67db1754d0c61671798264
Vivian Zitek <vivian.zitek@gmail.com>
2022-12-07 13:58:37 -0700

    Remove test code from Program.cs

--- Program.cs
+++ Program.cs
@@ -2,19 +2,9 @@
 // By: Jasmine Carrasco, Chad Miller, and Pluto Zitek
 using userinput;
 
-// UserInput.Input();
+UserInput.Input();
 
-// // What are the color harmonies?
-// ColorHarmony.Colorharmonypick();
-// ColorHarmony.ColorHarmonyInfo();
-// ColorHarmony.Menu();
-var inputRGB = (167, 110, 220);
-Console.WriteLine("RGB to HEX");
-Console.WriteLine(ConvertMode.RGBtoHEX(inputRGB));
-Console.WriteLine("");
-var inputHSV = (271, 50.0, 86.3);
-Console.WriteLine("HSV to HEX");
-Console.WriteLine(ConvertMode.HSVtoHEX(inputHSV));
-var inputHSL = (271, 61.1, 64.7);
-Console.WriteLine("HSL to HEX");
-Console.WriteLine(ConvertMode.HSLtoHEX(inputHSL));
+// What are the color harmonies?
+ColorHarmony.Colorharmonypick();
+ColorHarmony.ColorHarmonyInfo();
+ColorHarmony.Menu();
